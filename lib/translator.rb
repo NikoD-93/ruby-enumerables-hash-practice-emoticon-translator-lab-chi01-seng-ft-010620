8b69a29eeb1edf5627a7632948865bebbe11e331
Kieron Niko Dixon <nikodixon@gmail.com>
2020-01-03 19:50:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,31 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+def load_library(library)
+translator = { "get_meaning" => {}, "get_emoticon" => {}
+}
+YAML.load_file(library).each { |meaning, trans|
+ jpn = trans [1]
+ eng = trans[0]
+     translator["get_meaning"][jpn] = meaning
+     translator["get_emoticon"][eng] = jpn
+}
+  translator
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+# eng = trans[0]
+# jpn = trans[1]
+# translator["get_meaning"][eng] = jpn 
+# translator["get_emoticon"][jpn] = meaning 
+# }
+# translator
+# # end
+
+def get_japanese_emoticon(path, emoticon)
+  load_library(path)
+  
+  
+  
 end
 
 def get_english_meaning
