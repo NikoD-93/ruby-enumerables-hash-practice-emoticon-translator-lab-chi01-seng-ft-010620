f9b97dbb3ac02149e1622eac3a9841eaae790053
Kieron Niko Dixon <nikodixon@gmail.com>
2020-01-03 20:00:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,6 +34,6 @@ def get_japanese_emoticon(path, emoticon)
   
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, japanese_emoticon)
+  the_meanings = load_library(path)
 end
